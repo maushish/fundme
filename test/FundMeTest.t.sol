4dca5746fe7886dc14cb966006f9839d9981d767
Maushish <chamarnumber102@gmail.com>
2023-08-31 06:49:44 +0530

    More

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -23,4 +23,44 @@ contract FundMeTest is Test {
         uint256 version=fundMe.getVersion();
         assertEq(version, 4);
     }
+    function testFundFailsWithoutEnoughETH()public{
+        vm.expectRevert(); //the next line should revert!
+        //assert(this tx fails/reverts)
+        fundMe.fund();//send 0 value;
+    }
+    function testFundUpdatesFUndedDataStruct() public{
+        fundMe.fund{value:10e18}();
+        
+    }//getters functions (view/pure)
+    function testFunUpdatesFUndedDataStructure() public{
+        vm.prank(USER);//the next tx will be sent by user
+        fundMe.fund{value:10e18}();
+        uint256 amountFunded=fundMe.getAddressToAmountFunded(USER);
+        assrtEq(amountFunded,10e18);
+    }   //use deal cheatcode to send fake money
+    modifier funded(){
+        vm.prank(USER);
+        fundme.fund{value:SEND_VALUE};
+        _;
+    }
+
+    function testAddsFunderTOarrayOfFunders()public{
+        vm.prank(USER);
+        fundMe.fund{value:SEND_VALUE}();
+        adress funder=fundMe.getFUnder(0);
+        assertEq(funder, USER);
+    }   
+    function  testWithDrawWithASingleFunder() public funded{
+        //Arrange
+        uint256 startingOwnerBalance=fundMe.getOwner().balance;
+        // act 
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+        //assert 
+        uint256 endingOwnerBalance= fundMe.getOwner().balance;
+        uint256 endingFundMeBalance=address(fundME).balance;
+        assertEq(endingFundMeBalance,0);
+
+
+    }
 }
